b3826877298767797aa40344fa80b3fa5bd836dc
Julie Nisbet <jmnisbet@gmail.com>
2020-07-21 19:11:37 -0700

    using Promise.all

--- poke.js
+++ poke.js
@@ -1,35 +1,35 @@
-document.addEventListener("DOMContentLoaded", e => {
-    const pokeURL = `https://pokeapi.co/api/v2/pokemon/?limit=10`;
-    const pokemon = [];
+document.addEventListener('DOMContentLoaded', e => {
+    const pokeURL = `https://pokeapi.co/api/v2/pokemon/?limit=10`
+    const pokemon = []
     // go to the pokeURL
     // parse the json response
     // store the information in variable pokemon
-    const appendHTML = function(){
-        const pokeList = document.getElementById("poke-list")
-        pokemon.forEach(elem => {
-            fetch(elem.url)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    const pokeName = document.createElement("span")
-                    pokeName.innerHTML = data.name;
 
-                    const pokeImage = document.createElement("img");
-                    pokeImage.src = data.sprites.front_shiny;
+    const appendHTML = function(response) {
+        response.then(data => {
+            const pokeList = document.getElementById('poke-list')
+            const pokeName = document.createElement('span')
+            pokeName.innerHTML = data.name
 
-                    const pokeElem = document.createElement("li");
-                    pokeElem.appendChild(pokeImage)
-                    pokeElem.appendChild(pokeName)
+            const pokeImage = document.createElement('img')
+            pokeImage.src = data.sprites.front_shiny
 
-                    pokeList.appendChild(pokeElem)
+            const pokeElem = document.createElement('li')
+            pokeElem.appendChild(pokeImage)
+            pokeElem.appendChild(pokeName)
 
+            pokeList.appendChild(pokeElem)
+        })
+    }
 
-                })
-            })       
+    const getPokeDetail = function() {
+        const pokePromises = pokemon.map(elem => fetch(elem.url))
+        return Promise.all(pokePromises)
     }
 
     fetch(pokeURL)
         .then(response => response.json())
         .then(data => pokemon.push(...data.results))
-        .then(appendHTML)
+        .then(getPokeDetail)
+        .then(files => files.forEach(file => appendHTML(file.json())))
 })
